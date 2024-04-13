05de0b4d846e763b1614e2004140684e51253bed
calvin-mcd <114294748+calvin-mcd@users.noreply.github.com>
2024-04-13 22:09:12 +0200

    firmware

---
+++ Firmware/Ortho/eightu_ortho/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0E, 0xBB, 0x9B, 0x62, 0xD6, 0x78, 0xFD, 0xBE}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
+
+#define VIAL_COMBO_ENTRIES 69
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define TAPPING_TERM 250
+
